2bd0ded29f8aa3ecaf82986c2db80a1af0067307
Kai Zhang <kai@kzhang.org>
2021-12-03 23:00:25 -0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,34 @@ use itertools::Itertools;
 use itertools::Groups;                                                                                                          
 use csv::Reader;
 
-fn read_tss<R: Read>(file: R) {
-
+fn read_tss<R: Read>(file: R) -> impl Iterator<Item = (String, u32, bool)> {
+    let mut reader = BufReader::new(file);
+    let parseLine = |line: io::Result<String>| {
+        let chr_idx: usize = 0;
+        let type_idx: usize = 2;
+        let start_idx: usize = 3;
+        let end_idx: usize = 4;
+        let strand_idx: usize = 6;
+        let l = line.unwrap();
+        if l.as_bytes()[0] as char == '#' {
+            return None;
+        }
+        let elements: Vec<&str> = l.split('\t').collect();
+        if elements[type_idx] == "transcript" {
+            let chr = elements[chr_idx].to_string();
+            let is_fwd = elements[strand_idx] != "-";
+            let tss: u32 = 
+                if is_fwd {
+                    elements[start_idx].parse::<u32>().unwrap() - 1
+                } else {
+                    elements[end_idx].parse::<u32>().unwrap() - 1
+                };
+            Some((chr, tss, is_fwd))
+        } else {
+            None
+        }
+    };
+    reader.lines().filter_map(parseLine)
 }
 
 fn read_fragments<R: Read>(file: R) -> impl Iterator<Item = bed::Record> {
@@ -57,9 +83,9 @@ mod tests {
     #[test]
     fn it_works() {
         let f = File::open("data/fragments.bed.gz").expect("xx");
-
-        //for x in super::read_fragments(GzDecoder::new(f)) {
-        //    println!("{}", x.chrom());
-        //}
+        let gencode = File::open("data/gencode.gtf.gz").expect("xx");
+        for x in super::read_tss(GzDecoder::new(gencode)) {
+            println!("{}\t{}\t{}", x.0, x.1, x.2);
+        }
     }
 }
