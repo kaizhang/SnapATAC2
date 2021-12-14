38ddc66d845355f3ba656fac15bc66053c37ba87
Kai Zhang <kai@kzhang.org>
2021-12-13 20:26:19 -0800

    add benchmarks

--- bed-utils/src/lib.rs
+++ bed-utils/src/lib.rs
@@ -1,3 +1,10 @@
+use noodles_bed as bed;
+use bio::data_structures::interval_tree;
+
+use std::collections::HashMap;
+
+pub struct BedTree<T>(HashMap<String, interval_tree::IntervalTree<u64, T>>);
+
 #[cfg(test)]
 mod tests {
     #[test]
