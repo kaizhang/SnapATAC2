1954df6bec2b3125b5c76575f1e369e9741bdb1b
Kai Zhang <kai@kzhang.org>
2021-12-31 20:55:35 -0800

    minor

--- tsse/examples/create_hdf5.rs
+++ tsse/examples/create_hdf5.rs
@@ -12,5 +12,7 @@ fn main() -> Result<()> {
     let group = file.create_group("X")?;
     let data = ResizableVectorData::new(&group, "vec", 10000)?;
     data.extend(vec.into_iter())?;
+    group.new_attr_builder().with_data(&arr1(&[2, 3])).create("shape")?;
+    create_str_attr(&group, "encoding-type", "csr_matrix")?;
     Ok(())
 }
