8608f00b1e2545f48e00925d8de4656aa9369dd5
Kai Zhang <kai@kzhang.org>
2022-01-04 14:53:13 -0800

    add python bindings

---
+++ snapatac2-python/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+setup(
+    name="snapatac2",
+    description='SnapATAC: Single Nucleus Analysis Pipeline for ATAC-seq',
+    url='', 
+    author='Kai Zhang',
+    author_email='kai@kzhang.org',
+    license='MIT',
+    version="0.1.0",
+    rust_extensions=[RustExtension("snapatac2.snapatac2", binding=Binding.PyO3)],
+    packages=["snapatac2"],
+    zip_safe=False,
+    python_requires=">=3.6",
+    install_requires=['numpy>=1.16.0'],
+)
---
+++ snapatac2-python/src/lib.rs
@@ -0,0 +1,32 @@
+use pyo3::prelude::*;
+
+use bed_utils::bed::{BED, BEDLike, tree::BedTree, io::Reader};
+use itertools::{Itertools, GroupBy};
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use flate2::read::GzDecoder;
+
+use snapatac2_core::qc;
+
+#[pyfunction]
+fn get_qc(output_file: &str, gtf_file: &str, fragment_file: &str) -> PyResult<()> {
+    let gtf = GzDecoder::new(File::open(gtf_file).expect("fail to open the GTF file"));
+    let fragment = GzDecoder::new(File::open(fragment_file).expect("fail to open the fragment file"));
+    let promoter = qc::make_promoter_map(qc::read_tss(gtf));
+    let output = File::create(output_file).expect("Unable to create output file");
+    let mut buffer = BufWriter::new(output);
+    write!(buffer, "Barcode\tTSSe\tNum_unique_fragment\tFraction_mito\tFraction_duplicated\n").unwrap();
+    for (bc, fragments) in qc::read_fragments(fragment).into_iter() {
+        let qc = qc::get_qc(&promoter, fragments);
+        write!(buffer, "{}\t{}\t{}\t{}\t{}\n", bc, qc.tss_enrichment,
+            qc.num_unique_fragment, qc.frac_mitochondrial, qc.frac_duplicated).unwrap();
+    }
+    Ok(())
+}
+
+#[pymodule]
+fn snapatac2(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(get_qc, m)?)?;
+
+    Ok(())
+}
