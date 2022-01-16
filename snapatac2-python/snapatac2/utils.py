1893e36b2e74ee474fea52c112da3b52b2091b66
Kai Zhang <kai@kzhang.org>
2022-01-15 16:26:47 -0800

    update

---
+++ snapatac2-python/snapatac2/utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+import scipy.sparse as ss
+import anndata as ad
+
+def read_as_binarized(adata: ad.AnnData) -> ss.spmatrix:
+    grp = adata.file["X"]
+    mtx = ss.csr_matrix(adata.shape, dtype=np.int8)
+    mtx.indices = grp["indices"][...]
+    mtx.indptr = grp["indptr"][...]
+    mtx.data = np.ones(mtx.indices.shape, dtype=np.int8)
+    return mtx
