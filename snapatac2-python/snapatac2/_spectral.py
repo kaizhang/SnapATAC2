84f29c7295f2c9cbc562235581d9ef5fb224ec45
Kai Zhang <kai@kzhang.org>
2022-01-21 21:44:32 -0800

    optimize spectral embedding; reduce memory usage

--- snapatac2-python/snapatac2/_spectral.py
+++ snapatac2-python/snapatac2/_spectral.py
@@ -4,6 +4,9 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics.pairwise import cosine_similarity, rbf_kernel
 import bz2
 import _pickle as cPickle
+import gc
+
+from .snapatac2 import jm_regress
 
 def compressed_pickle(title, data):
     with bz2.BZ2File(title, "w") as f:
@@ -21,7 +24,6 @@ class Spectral:
         self.n_dim = n_dim
         self.distance = distance
         if (self.distance == "jaccard"):
-            print("Use jaccard distance")
             self.compute_similarity = jaccard_similarity
         elif (self.distance == "cosine"):
             self.compute_similarity = cosine_similarity
@@ -33,21 +35,19 @@ class Spectral:
         self.dim = mat.shape[1]
         self.coverage = mat.sum(axis=1) / self.dim
         print("Compute similarity matrix")
-        jm = self.compute_similarity(mat)
+        S = self.compute_similarity(mat)
 
         if (self.distance == "jaccard"):
-            self.normalizer = Normalizer(jm, self.coverage)
-            S = self.normalizer.predict(jm, self.coverage, self.coverage)
-        else:
-            S = jm
+            print("Normalization")
+            self.normalizer = JaccardNormalizer(S, self.coverage)
+            self.normalizer.normalize(S, self.coverage, self.coverage)
 
         np.fill_diagonal(S, 0)
-        print("Normalization")
-        self.D = np.diag(1/(self.sampling_rate * S.sum(axis=1)))
-        L = np.matmul(self.D, S)
+        d = 1 / (self.sampling_rate * S.sum(axis=1))
+        np.multiply(S, d, out=S)
 
-        print("Reduction")
-        evals, evecs = sp.sparse.linalg.eigs(L, self.n_dim + 1, which='LR')
+        print("Perform decomposition")
+        evals, evecs = sp.sparse.linalg.eigs(S, self.n_dim + 1, which='LR')
         ix = evals.argsort()[::-1]
         self.evals = np.real(evals[ix])
         self.evecs = np.real(evecs[:, ix])
@@ -55,36 +55,59 @@ class Spectral:
     def predict(self, data=None):
         if data == None:
             return self.evecs
-        jm = self.compute_similarity(self.sample, data)
+        S = self.compute_similarity(self.sample, data)
 
         if (self.distance == "jaccard"):
-            S_ = self.normalizer.predict(jm, self.coverage, data.sum(axis=1) / self.dim).T
-        else:
-            S_ = jm.T
+            self.normalizer.normalize(S, self.coverage, data.sum(axis=1) / self.dim)
+        S = S.T
 
-        D_ = np.diag(1/(self.sampling_rate * S_.sum(axis=1)))
-        L_ = np.matmul(D_, S_)
-        evecs = (L_.dot(self.evecs)).dot(np.diag(1/self.evals))
+        d = 1 / (self.sampling_rate * S.sum(axis=1))
+        np.multiply(S, d, out=S)
+
+        evecs = (S.dot(self.evecs)).dot(np.diag(1/self.evals))
         return evecs
 
-class Normalizer:
+def jaccard_similarity(m1, m2=None):
+    """Compute pair-wise jaccard index
+
+    Args:
+        mat1: n1 x m
+        mat2: n2 x m
+    
+    Returns:
+        Jaccard similarity matrix
+    """
+    s1 = m1.sum(axis=1)
+    if m2 is None:
+        d = m1.dot(m1.T).todense()
+        gc.collect()
+        t = np.negative(d)
+        t += s1
+        t += s1.T
+        d /= t
+    else:
+        s2 = m2.sum(axis=1)
+        d = m1.dot(m2.T).todense()
+        gc.collect()
+        d /= -d + s1 + s2.T
+    gc.collect()
+    return d
+
+class JaccardNormalizer:
     def __init__(self, jm, c):
-        n, _ = jm.shape
+        (slope, intersect) = jm_regress(jm, c)
+        self.slope = slope
+        self.intersect = intersect
 
-        X = 1 / c.dot(np.ones((1,n)))
-        X = 1 / (X + X.T - 1)
-        X = X[np.triu_indices(n, k = 1)].T
-        y = jm[np.triu_indices(n, k = 1)].T
-
-        self.model = LinearRegression().fit(X, y)
-
-    def predict(self, jm, c1, c2):
-        X1 = 1 / c1.dot(np.ones((1, c2.shape[1]))) 
-        X2 = 1 / c2.dot(np.ones((1, c1.shape[1])))
-        X = 1 / (X1 + X2.T - 1)
-
-        y = self.model.predict(X.flatten().T).reshape(jm.shape)
-        return np.array(jm / y)
+    def normalize(self, jm, c1, c2):
+        # jm / (self.slope / (1 / c1 + 1 / c2.T - 1) + self.intersect)
+        temp = 1 / c1 + 1 / c2.T
+        temp -= 1
+        np.reciprocal(temp, out=temp)
+        np.multiply(temp, self.slope, out=temp)
+        temp += self.intersect
+        jm /= temp
+        gc.collect()
 
 def old_jaccard_similarity(mat1, mat2=None):
     """Compute pair-wise jaccard index
@@ -111,27 +134,70 @@ def old_jaccard_similarity(mat1, mat2=None):
         jm = jm / (c + c.T - jm)
     return jm
 
-# regress out a variable
-def regress(X, y):
-    model = LinearRegression().fit(X, y)
-    return model.predict(X)
+class Old_JaccardNormalizer:
+    def __init__(self, jm, c):
+        n, _ = jm.shape
 
-def jaccard_similarity(m1, m2=None):
-    """Compute pair-wise jaccard index
+        X = 1 / c.dot(np.ones((1,n)))
+        X = 1 / (X + X.T - 1)
+        X = X[np.triu_indices(n, k = 1)].T
+        y = jm[np.triu_indices(n, k = 1)].T
 
-    Args:
-        mat1: n1 x m
-        mat2: n2 x m
-    
-    Returns:
-        Jaccard similarity matrix
-    """
-    s1 = m1.sum(axis=1)
-    if m2 is None:
-        d = m1.dot(m1.T).todense()
-        d /= -d + s1 + s1.T
-    else:
-        s2 = m2.sum(axis=1)
-        d = m1.dot(m2.T).todense()
-        d /= -d + s1 + s2.T
-    return d
+        self.model = LinearRegression().fit(X, y)
+
+    def predict(self, jm, c1, c2):
+        X1 = 1 / c1.dot(np.ones((1, c2.shape[1]))) 
+        X2 = 1 / c2.dot(np.ones((1, c1.shape[1])))
+        X = 1 / (X1 + X2.T - 1)
+
+        y = self.model.predict(X.flatten().T).reshape(jm.shape)
+        return np.array(jm / y)
+
+class Old_Spectral:
+    def __init__(self, n_dim=30, sampling_rate=1, distance="jaccard"):
+        self.sampling_rate = sampling_rate
+        #self.dim = mat.get_shape()[1]
+        self.n_dim = n_dim
+        self.distance = distance
+        if (self.distance == "jaccard"):
+            print("Use jaccard distance")
+            self.compute_similarity = old_jaccard_similarity
+        elif (self.distance == "cosine"):
+            self.compute_similarity = cosine_similarity
+        else:
+            self.compute_similarity = rbf_kernel
+
+    def fit(self, mat):
+        self.sample = mat
+        self.dim = mat.shape[1]
+        self.coverage = mat.sum(axis=1) / self.dim
+        S = self.compute_similarity(mat)
+
+        if (self.distance == "jaccard"):
+            self.normalizer = Old_JaccardNormalizer(S, self.coverage)
+            S = self.normalizer.predict(S, self.coverage, self.coverage)
+
+        np.fill_diagonal(S, 0)
+        D = np.diag(1/(self.sampling_rate * S.sum(axis=1)))
+        L = np.matmul(D, S)
+
+        evals, evecs = sp.sparse.linalg.eigs(L, self.n_dim + 1, which='LR')
+        ix = evals.argsort()[::-1]
+        self.evals = np.real(evals[ix])
+        self.evecs = np.real(evecs[:, ix])
+
+    def predict(self, data=None):
+        if data == None:
+            return self.evecs
+        jm = self.compute_similarity(self.sample, data)
+
+        if (self.distance == "jaccard"):
+            S_ = self.normalizer.predict(jm, self.coverage, data.sum(axis=1) / self.dim).T
+        else:
+            S_ = jm.T
+
+        D_ = np.diag(1/(self.sampling_rate * S_.sum(axis=1)))
+        L_ = np.matmul(D_, S_)
+        evecs = (L_.dot(self.evecs)).dot(np.diag(1/self.evals))
+        return evecs
+
--- snapatac2-python/src/lib.rs
+++ snapatac2-python/src/lib.rs
@@ -1,10 +1,14 @@
 use pyo3::prelude::*;
+use pyo3::types::PyIterator;
+use numpy::{PyReadonlyArrayDyn};
+use pyo3::{pymodule, types::PyModule, PyResult, Python};
 
 use bed_utils::bed::GenomicRange;
 use std::fs::File;
 use std::collections::BTreeMap;
 use flate2::read::GzDecoder;
 use hdf5;
+use linreg::lin_reg_imprecise;
 
 use snapatac2_core::{create_tile_matrix, qc};
 
@@ -58,9 +62,31 @@ fn is_gzipped(file: &str) -> bool {
     GzDecoder::new(File::open(file).unwrap()).header().is_some()
 }
 
+#[pyfunction]
+fn simple_lin_reg(py_iter: &PyIterator) -> PyResult<(f64, f64)> {
+    Ok(lin_reg_imprecise(py_iter.map(|x| x.unwrap().extract().unwrap())).unwrap())
+}
+
+
+#[pyfunction]
+fn jm_regress<'py>(
+    jm_: PyReadonlyArrayDyn<'_, f64>,
+    count_: PyReadonlyArrayDyn<'_, f64>,
+) -> PyResult<(f64, f64)> {
+    let jm = &jm_.as_array();
+    let n_row = jm.shape()[0];
+    let count = &count_.as_array();
+    let iter = (0..n_row).flat_map(|i| (i+1..n_row)
+        .map(move |j| (1.0 / (1.0 / count[[i, 0]] + 1.0 / count[[j, 0]] - 1.0), jm[[i, j]]))
+    );
+    Ok(lin_reg_imprecise(iter).unwrap())
+}
+
 #[pymodule]
 fn snapatac2(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(mk_tile_matrix, m)?)?;
+    m.add_function(wrap_pyfunction!(simple_lin_reg, m)?)?;
+    m.add_function(wrap_pyfunction!(jm_regress, m)?)?;
 
     Ok(())
 }
