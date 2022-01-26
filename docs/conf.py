5014c90dce67fdc49bcb699a3dfa2d581ee2f97e
Kai Zhang <kai@kzhang.org>
2022-01-26 11:35:37 -0800

    update doc

--- docs/conf.py
+++ docs/conf.py
@@ -6,14 +6,9 @@
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+#import os
+#import sys
+#sys.path.insert(0, os.path.abspath('../snapatac2-python'))
 
 # -- Project information -----------------------------------------------------
 
@@ -27,17 +22,54 @@ release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 
+needs_sphinx = '2.0'  # Nicer param docs
+suppress_warnings = ['ref.citation']
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.doctest',
-    'autoapi.extension',
     'nbsphinx',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
 ]
 
-autoapi_type = 'python'
-autoapi_dirs = ['../snapatac2-python/snapatac2']
+# Generate the API documentation when building
+autosummary_generate = True
+autodoc_member_order = 'bysource'
+# autodoc_default_flags = ['members']
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_use_rtype = True  # having a separate entry generally helps readability
+napoleon_use_param = True
+napoleon_custom_sections = [('Params', 'Parameters')]
+todo_include_todos = False
+
+intersphinx_mapping = {
+    "anndata": ("https://anndata.readthedocs.io/en/stable/", None),
+    "cycler": ("https://matplotlib.org/cycler/", None),
+    "h5py": ("http://docs.h5py.org/en/stable/", None),
+    "ipython": ("https://ipython.readthedocs.io/en/stable/", None),
+    "louvain": ("https://louvain-igraph.readthedocs.io/en/latest/", None),
+    "matplotlib": ("https://matplotlib.org/", None),
+    "networkx": (
+        "https://networkx.github.io/documentation/networkx-1.10/",
+        None,
+    ),
+    "numpy": ("https://docs.scipy.org/doc/numpy/", None),
+    "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
+    "pytest": ("https://docs.pytest.org/en/latest/", None),
+    "python": ("https://docs.python.org/3", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/reference/", None),
+    "seaborn": ("https://seaborn.pydata.org/", None),
+    "sklearn": ("https://scikit-learn.org/stable/", None),
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- snapatac2-python/snapatac2/__init__.py
+++ snapatac2-python/snapatac2/__init__.py
@@ -1,6 +1,12 @@
-from . import preprocessing
+from . import preprocessing as pp
 from . import embedding
 from . import cluster
 
-from anndata import AnnData, concat
+from anndata import AnnData, concat, read, read_h5ad
 from anndata.experimental import AnnCollection
+
+import sys
+
+sys.modules.update({f'{__name__}.{m}': globals()[m] for m in ['pp']})
+
+del sys
