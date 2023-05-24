0d67f9a84f2e018712ae814eb8aef51e4dab3f43
beyondpie <panshi.pie@gmail.com>
2023-05-24 15:30:23 -0700

    add cemba_metrics implementation

--- snapatac2-contrib/clustering_metric/cemba_metrics.py
+++ snapatac2-contrib/clustering_metric/cemba_metrics.py
@@ -1,23 +1,34 @@
 # envs from CEMBA consensus analysis.
 import os
 import scipy
-import leidenalg as la
-import igraph as ig
-from scipy.io import mmread
-from scipy import sparse
-from time import perf_counter as pc
-from scipy.sparse import csr_matrix, lil_matrix
-from scipy.sparse import save_npz
+import random
+from typing import Tuple, Union
+from scipy.sparse import csr_matrix
 import numpy as np
 import itertools
-from scipy.cluster.hierarchy import linkage, leaves_list, cophenet
+from scipy.cluster.hierarchy import linkage, leaves_list
 from scipy.spatial.distance import squareform
 
+import leidenalg as la
+import igraph as ig
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import savefig, imshow, set_cmap
-from multiprocessing import Pool
-plt.switch_backend("agg")
-import fastcluster as fc
+
+def cal_connectivity(P):
+    """calculate connectivity matrix"""
+    # print("=== calculate connectivity matrix ===")
+    # connectivity_mat = lil_matrix((len(P), len(P)))
+    # connectivity_mat = csr_matrix((len(P), len(P)))
+    connectivity_mat = np.zeros((len(P), len(P)), dtype = bool)
+    classN = max(P)
+    ## TODO: accelerate this
+    for cls in range(int(classN + 1)):
+        xidx = [i for i, x in enumerate(P) if x == cls]
+        iterables = [xidx, xidx]
+        for t in itertools.product(*iterables):
+            connectivity_mat[t[0], t[1]] = True
+    """connectivity_mat = csr_matrix(connectivity_mat)"""
+    return connectivity_mat
+
 
 def reorder(C):
     """
@@ -51,76 +62,74 @@ def plot_CDF(prefix, C, u1, u2, num_bins=100):
 
 def cal_dispersion(C):
     """calculate dispersion coefficient"""
-    print("=== calculate dispersion coefficient ===")
-    start_t = pc()
     n = C.shape[1]
-    corr_disp = np.sum(4 * np.square(np.concatenate(C - 1 / 2))) / (np.square(n))
-    end_t = pc()
-    print("Used (secs): ", end_t - start_t)
+    corr_disp = np.sum(
+        4 * np.square(C - 0.5), dtype = np.float64) / (np.square(n))
     return corr_disp
 
 
 def cal_PAC(C, u1, u2):
     """calculate PAC (proportion of ambiguous clustering)"""
-    print("=== calculate PAC (proportion of ambiguous clustering) ===")
-    start_t = pc()
-    totalN = C.shape[0] * C.shape[0]
-    u1_fraction = (C.ravel() < u1).sum() / totalN
-    u2_fraction = (C.ravel() < u2).sum() / totalN
-    PAC = u2_fraction - u1_fraction
-    end_t = pc()
-    print("Used (secs): ", end_t - start_t)
+    n = C.shape[0] ** 2
+    PAC = ((C<u2).sum() - (C<u1).sum()) / n
     return PAC
 
-def cal_stab(x):
+def cal_stab(x) -> np.ndarray:
     """calculate stability for every cell"""
     s = np.sum(abs(x - 0.5)) / (0.5 * x.shape[0])
     return s
 
 
-
-def run(knn :csr_matrix,
-        reso: float = 1.0,
-        left_cutoff: float = 0.05,
-        right_cutoff:float = 0.95,
-        niter: int = 10,
-        nsample: int = 50000) -> float:
+def cemba_metric(
+        partitions: np.ndarray,
+        nsample: Union[int, None] = None
+)->Tuple[float, float]:
     """
     Parameters
     ----------
-    knn
-        k-nearst neighbor graph
+    partitions
+        numpy.ndarray, shape n_cell x n_times
     Returns
     -------
-    
+    float
+         cemba metric for clustering
     """
-    return 1.0
+    # * check partitions
+    ndim:int = partitions.ndim
+    if ndim != 2:
+        raise RuntimeError(
+            f"partitions should have 2 instead of {ndim} dims.")
+    # * to unsigned int64
+    p = partitions.astype(np.uint)
+    n_cell, n_times = p.shape
+    # * downsample partitions if needed.
+    if nsample and n_cell > nsample:
+        print(f"Down sampling {n_cell} to {nsample}")
+        random.seed(0)
+        index = random.sample(range(n_cell), nsample)
+        p = p[index, :]
+    consensus = np.zeros((n_cell, n_cell), dtype = np.float16)
+    for i in range(n_times):
+        print(f"{i} / {n_times} for consensus matrix")
+        conn = cal_connectivity(p[:,i])
+        consensus += conn
+    consensus /= n_times
+    disp: float = cal_dispersion(consensus)
+    pac: float = cal_PAC(
+        consensus, u1 = 0.05, u2 = 0.95)
+    # stabs per cell
+    # stabs: np.ndarray = np.apply_along_axis(cal_stab, 0, consensus)
+    return (disp, pac)
 
-
-# * test
-knn = scipy.io.mmread(os.path.join(
+if __name__ == '__main__':
+    # * test
+    knn = scipy.io.mmread(os.path.join(
         "test_data", "GLUTL3", "GLUT_13",
         "GLUT_13.k-50.km-RANN.mmtx"
-))
-knn = knn.tocsr()
-
-# * run in 02.consensusAnalysis.py
-def run():
-    """Run and count to peak"""
-    start_time = pc()
-    """ init input files """
-    inf = args.input
-    reso = args.resolution
-    N = args.N
-    sampleN = args.sample
-    u1 = args.u1
-    u2 = args.u2
-    outf = args.output
-
-    knn = mmread(inf)
+    ))
     knn = knn.tocsr()
+    reso = 0.8
     dimN = knn.shape[0]
-
     vcount = max(knn.shape)
     sources, targets = knn.nonzero()
     edgelist = list(zip(sources.tolist(), targets.tolist()))
@@ -128,56 +137,17 @@ def run():
 
     # generate consensus matrix
     dims = knn.shape[0]
-    if dims > sampleN:
-        dims = sampleN
-        import random
-
-        random.seed(2022)
-        idxy = random.sample(range(dimN), sampleN)
-        idxy = sorted(idxy)
     consensus = csr_matrix((dims, dims))
-
-    print("=== calculate connectivity matrix ===")
-
-    for seed in range(N):
-        start_t =pc()
-        partition = la.find_partition(g, la.RBConfigurationVertexPartition, resolution_parameter = reso, seed = seed)
+    N = 2
+    def do_leiden(seed):
+        partition = la.find_partition(
+            g, la.RBConfigurationVertexPartition,
+            resolution_parameter = reso,
+            seed = seed)
         part_membership = partition.membership
-        part_membership = np.array(part_membership)
-        if len(part_membership) > sampleN:
-            part_membership = part_membership[idxy] # downsample (10,000 observations)
-        outs = cal_connectivity(part_membership)
-        consensus += outs
-        end_t = pc()
-        print('seed ', seed, ' used (secs): ', end_t - start_t)
-    consensus /= N
+        return(part_membership)
 
-    # save consensus matrix
-    consensus_sp = sparse.csr_matrix(consensus)
-    outfname = ".".join([outf, "consensus", "npz"])
-    save_npz(outfname, consensus_sp)
+    partitions = list(map(do_leiden, range(N)))
+    partitions = np.transpose(np.array(partitions))
+    cemba_metric(partitions, nsample = N)
 
-    # plotting
-    order_consensus = reorder(consensus)
-    plotC(outf, order_consensus)
-    plot_CDF(outf, consensus, u1, u2)
-
-    # cal measurement
-    #    o_cophcor = cal_cophenetic(consensus)
-    o_disp = cal_dispersion(consensus)
-    o_PAC = cal_PAC(consensus, u1, u2)
-    print("=== calculate stability for every cell ===")
-    o_stab = np.apply_along_axis(cal_stab, 0, consensus)
-
-    # write stat
-    out_list = [outf, reso, o_disp, o_PAC]
-    outstat = ".".join([outf, "stat.txt"])
-    with open(outstat, "w") as fo:
-        fo.write("\t".join(str(i) for i in out_list) + "\n")
-
-    outStab = ".".join([outf, "stab.txt"])
-    with open(outStab, "w") as fo:
-        fo.write("\t".join(str(i) for i in o_stab) + "\n")
-
-    end_time = pc()
-    print("Total used (secs): ", end_time - start_time)
